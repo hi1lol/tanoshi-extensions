4d5878441ef4435100cea5d8828cd4863bb19700
Luis Garcia <git@luigi311.com>
2026-07-11 14:58:58 +0000

    extension-utils: Add shared extension boilerplate macros

---
+++ common/extension-utils/src/lib.rs
@@ -0,0 +1,72 @@
+use tanoshi_lib::prelude::{Extension, Input, PluginRegistrar};
+
+#[doc(hidden)]
+pub use anyhow;
+#[doc(hidden)]
+pub use tanoshi_lib;
+
+/// Merge preference updates into an extension's declared preferences.
+///
+/// Matching deliberately uses [`Input::eq`]. In `tanoshi-lib` 0.38.0,
+/// `PartialEq` matches the input variant and name while ignoring its state and
+/// available values. Updates for undeclared preferences are therefore ignored.
+pub fn merge_preferences(preferences: &mut [Input], updates: Vec<Input>) {
+    for update in updates {
+        for preference in preferences.iter_mut() {
+            if update.eq(preference) {
+                *preference = update.clone();
+            }
+        }
+    }
+}
+
+#[doc(hidden)]
+pub fn register_extension<E>(
+    registrar: &mut dyn PluginRegistrar,
+    name: &str,
+    version: &str,
+    log_target: &str,
+) where
+    E: Default + Extension + 'static,
+{
+    networking::init_plugin_logging();
+    log::info!(target: log_target, "Registering {name} extension v{version}");
+    registrar.register_function(Box::new(E::default()));
+}
+
+/// Generate the exported plugin declaration and its registration function.
+#[macro_export]
+macro_rules! export_extension {
+    ($register:ident, $extension:ty, $name:expr $(,)?) => {
+        $crate::tanoshi_lib::export_plugin!($register);
+
+        fn $register(registrar: &mut dyn $crate::tanoshi_lib::prelude::PluginRegistrar) {
+            $crate::register_extension::<$extension>(
+                registrar,
+                $name,
+                env!("CARGO_PKG_VERSION"),
+                module_path!(),
+            );
+        }
+    };
+}
+
+/// Generate `Extension` preference methods backed by a `Vec<Input>` field.
+#[macro_export]
+macro_rules! impl_preferences {
+    ($field:ident $(,)?) => {
+        fn set_preferences(
+            &mut self,
+            preferences: ::std::vec::Vec<$crate::tanoshi_lib::prelude::Input>,
+        ) -> $crate::anyhow::Result<()> {
+            $crate::merge_preferences(&mut self.$field, preferences);
+            ::core::result::Result::Ok(())
+        }
+
+        fn get_preferences(
+            &self,
+        ) -> $crate::anyhow::Result<::std::vec::Vec<$crate::tanoshi_lib::prelude::Input>> {
+            ::core::result::Result::Ok(self.$field.clone())
+        }
+    };
+}
